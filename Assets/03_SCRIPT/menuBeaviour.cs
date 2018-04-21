7ed9c10656f02123e0a3a63924be9f6f56abf7ea
Raiponz <spooki.leserpent@orange.fr>
2018-04-22 01:57:59 +0200

    endlevel menu

---
+++ Assets/03_SCRIPT/menuBeaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuBeaviour : MonoBehaviour {
+
+  
+
+public void ChangeLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Replay()
+    {
+        this.GetComponentInParent<Canvas>().enabled  = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
