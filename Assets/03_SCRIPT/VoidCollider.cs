e7bc698026bb4113f801a819549f99bc10e1735a
zessirb <zessirb@gmail.com>
2018-04-21 16:49:51 +0200

    Création d'un VoidCollider

---
+++ Assets/03_SCRIPT/VoidCollider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoidCollider : MonoBehaviour {
+
+    public Canvas canvas;
+
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Defeat();
+    }
+
+    private void Defeat()
+    {
+        Debug.Log("defeat");
+        canvas.enabled = true;
+    }
+}
