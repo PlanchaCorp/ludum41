98cdb59421509da09028618b58e6b3eedcb1a986
Raiponz <spooki.leserpent@orange.fr>
2018-04-22 03:00:14 +0200

    set text a the end of the level

--- Assets/03_SCRIPT/GameBehavior.cs
+++ Assets/03_SCRIPT/GameBehavior.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 
 public class GameBehavior : MonoBehaviour {
     private int mapScore;
-
+    public  int mapPar = 0;
+    public static int[] scores = new int[3];
     public void ResetScore()
     {
         mapScore = 0;
@@ -14,22 +15,67 @@ public class GameBehavior : MonoBehaviour {
         mapScore++;
         UpdateScore();
     }
+
     public int GetScore()
     {
         return mapScore;
     }
+
+    public void SetLevelScore(int level, int score)
+    {
+        scores[level] = score;
+    }
     
 	void Start () {
         ResetScore();
-	}
+        gameObject.GetComponent<GameBehavior>().UpdatePar();
+    }
 
     private void UpdateScore()
     {
         GameObject[] scoreTexts = GameObject.FindGameObjectsWithTag("ScoreText");
+        TextMeshProUGUI scoreCommentText = GameObject.Find("ScoreComment").GetComponent<TextMeshProUGUI>();
+        switch ( mapScore - mapPar)
+        {
+            case (2):
+                scoreCommentText.text = "Double Bogey";
+                break;
+            case (1):
+                scoreCommentText.text = "Bogey";
+                break;
+            case (0):
+                scoreCommentText.text = "Par";
+                break;
+            case (-1):
+                scoreCommentText.text = "Birdy";
+                break;
+            case (-2):
+                scoreCommentText.text = "Eagle";
+                break;
+            case (-3):
+                scoreCommentText.text = "Albatros";
+                break;
+
+        }
         foreach (GameObject scoreText in scoreTexts)
         {
+            Debug.Log(scoreText);
             TextMeshProUGUI text = scoreText.GetComponent<TextMeshProUGUI>();
             text.text = mapScore + "";
+
+            
         }
     }
+
+    public void UpdatePar()
+    {
+        GameObject[] parTexts = GameObject.FindGameObjectsWithTag("ParText");
+        foreach (GameObject parText in parTexts)
+        {
+            TextMeshProUGUI text = parText.GetComponent<TextMeshProUGUI>();
+            text.text = mapPar + "";
+        }
+    }
+
+
 }
--- Assets/03_SCRIPT/menuBeaviour.cs
+++ Assets/03_SCRIPT/menuBeaviour.cs
@@ -23,4 +23,6 @@ public void ChangeLevel()
         SceneManager.LoadScene("MainMenu");
     }
 
+   
+
 }
