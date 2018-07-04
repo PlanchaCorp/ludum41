8f62fc622b46d06798f726310a7bc2fd0288b6dc
zessirb <zessirb@gmail.com>
2018-07-04 13:11:48 +0200

    Fix credits

--- Assets/03_SCRIPT/CreditsFading.cs
+++ Assets/03_SCRIPT/CreditsFading.cs
@@ -20,29 +20,29 @@ public class CreditsFading : MonoBehaviour {
         timeStart = Time.time;
 	}
 	
-	void FixedUpdate ()
+	void Update ()
     {
-        float firstCanvasOpacity = (Time.time - 0) / (firstCanvasApparitionDelay - 0);
+        float firstCanvasOpacity = (Time.time - timeStart - 0) / (firstCanvasApparitionDelay - 0);
         if (firstCanvasOpacity >= 0 && firstCanvasOpacity <= 1)
             firstCanvas.GetComponent<CanvasGroup>().alpha = firstCanvasOpacity;
 
-        float firstCanvasOpacityWhenDisappearing = 1 - (Time.time - firstCanvasDisparitionDelay) / (secondCanvasApparitionDelay - firstCanvasDisparitionDelay);
+        float firstCanvasOpacityWhenDisappearing = 1 - (Time.time - timeStart - firstCanvasDisparitionDelay) / (secondCanvasApparitionDelay - firstCanvasDisparitionDelay);
         if (firstCanvasOpacityWhenDisappearing >= 0 && firstCanvasOpacityWhenDisappearing <= 1)
             firstCanvas.GetComponent<CanvasGroup>().alpha = firstCanvasOpacityWhenDisappearing;
 
-        float secondCanvasOpacity = (Time.time - firstCanvasDisparitionDelay) / (secondCanvasApparitionDelay - firstCanvasDisparitionDelay);
+        float secondCanvasOpacity = (Time.time - timeStart - firstCanvasDisparitionDelay) / (secondCanvasApparitionDelay - firstCanvasDisparitionDelay);
         if (secondCanvasOpacity >= 0 && secondCanvasOpacity <= 1)
             secondCanvas.GetComponent<CanvasGroup>().alpha = secondCanvasOpacity;
 
-        float secondCanvasOpacityWhenDisappearing = 1 - (Time.time - secondCanvasDisparitionDelay) / (thirdCanvasApparitionDelay - secondCanvasDisparitionDelay);
+        float secondCanvasOpacityWhenDisappearing = 1 - (Time.time - timeStart - secondCanvasDisparitionDelay) / (thirdCanvasApparitionDelay - secondCanvasDisparitionDelay);
         if (secondCanvasOpacityWhenDisappearing >= 0 && secondCanvasOpacityWhenDisappearing <= 1)
             secondCanvas.GetComponent<CanvasGroup>().alpha = secondCanvasOpacityWhenDisappearing;
 
-        float thirdCanvasOpacity = (Time.time - secondCanvasDisparitionDelay) / (thirdCanvasApparitionDelay - secondCanvasDisparitionDelay);
+        float thirdCanvasOpacity = (Time.time - timeStart - secondCanvasDisparitionDelay) / (thirdCanvasApparitionDelay - secondCanvasDisparitionDelay);
         if (thirdCanvasOpacity >= 0 && thirdCanvasOpacity <= 1)
             thirdCanvas.GetComponent<CanvasGroup>().alpha = thirdCanvasOpacity;
 
-        float thirdCanvasOpacityWhenDisappearing = 1 - (Time.time - thirdCanvasDisparitionDelay) / (nextSceneDelay - thirdCanvasDisparitionDelay);
+        float thirdCanvasOpacityWhenDisappearing = 1 - (Time.time - timeStart - thirdCanvasDisparitionDelay) / (nextSceneDelay - thirdCanvasDisparitionDelay);
         if (thirdCanvasOpacityWhenDisappearing >= 0 && thirdCanvasOpacityWhenDisappearing <= 1)
             thirdCanvas.GetComponent<CanvasGroup>().alpha = thirdCanvasOpacityWhenDisappearing;
 
